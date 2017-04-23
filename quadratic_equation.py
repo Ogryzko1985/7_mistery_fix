57e303b50cf6229b1e264f85d7e0dd03e7be2431
Константин Огрызко < >
2017-04-23 20:51:17 +0300

    Код отформатирован согласно pep8

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,23 +1,24 @@
 from math import sqrt
 a = int(input("Print a:"))
-print (a)
+print(a)
 b = int(input("Print b:"))
-print (b)
+print(b)
 c = int(input("Print c:"))
-print (c)
+print(c)
+
+
 def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
+    discriminant = b**2 - 4*a*c
     if discriminant < 0:
-        root1 = None 
+        root1 = None
         root2 = None
-        return root1, root2
+    return root1, root2
     else:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
-        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        root1 = (-b-sqrt(discriminant))/(2*a)
+        root2 = (-b+sqrt(discriminant))/(2*a)
     if discriminant == 0:
         return root1, None
     else:
         return root1, root2
-            
-f = get_roots(a,b,c)
+f = get_roots(a, b, c)
 print(f)

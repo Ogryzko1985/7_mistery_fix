615b231afab948def283347a88f7468a6795e1b7
Константин Огрызко < >
2017-04-26 22:22:55 +0300

    Исправлены замечания

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,24 +1,21 @@
 from math import sqrt
-a = int(input("Print a:"))
-print(a)
-b = int(input("Print b:"))
-print(b)
-c = int(input("Print c:"))
-print(c)
+parametr_a = int(input("Print parametr_a:"))
+parametr_b = int(input("Print parametr_b:"))
+parametr_c = int(input("Print parametr_c:"))
 
 
-def get_roots(a, b, c):
-    discriminant = b**2 - 4*a*c
+def get_roots(parametr_a, parametr_b, parametr_c):
+    discriminant = parametr_b**2 - 4*parametr_a*parametr_c
     if discriminant < 0:
         root1 = None
         root2 = None
         return root1, root2
     else:
-        root1 = (-b-sqrt(discriminant))/(2*a)
-        root2 = (-b+sqrt(discriminant))/(2*a)
+        root1 = (-parametr_b-sqrt(discriminant))/(2*parametr_a)
+        root2 = (-parametr_b+sqrt(discriminant))/(2*parametr_a)
     if discriminant == 0:
         return root1, None
     else:
         return root1, root2
-f = get_roots(a, b, c)
-print(f)
+    if __name__ == '__main__':
+        solution = get_roots(parametr_a, parametr_b, parametr_c)

63b8e4e7c3379540565242499f97daca446f642e
Константин Огрызко < >
2017-05-14 15:08:46 +0300

    Исправления от 14.05.2017

--- README.md
+++ README.md
@@ -9,8 +9,7 @@ If there is not any root  then the "none" word  is printed.
 before launching the program make sure the  program file "quadratic_equation.py" is in current directory printed in console.
 Use command "cd" to switch to directory the program file is in.
 In console write "python quadratic_equation.py"
-In console input after words "input(a,b,c)" any integer numbers.
-After enter program counts roots of the quadratic equation or returns "none" if any root is absent.
+
 
 # How to Launch Tests
 before launching tests make sure  the  program file "tests.py" is in current directory printed in console.
--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,21 +1,20 @@
 from math import sqrt
-parametr_a = int(input("Print parametr_a:"))
-parametr_b = int(input("Print parametr_b:"))
-parametr_c = int(input("Print parametr_c:"))
 
 
 def get_roots(parametr_a, parametr_b, parametr_c):
-    discriminant = parametr_b**2 - 4*parametr_a*parametr_c
-    if discriminant < 0:
-        root1 = None
-        root2 = None
-        return root1, root2
-    else:
-        root1 = (-parametr_b-sqrt(discriminant))/(2*parametr_a)
-        root2 = (-parametr_b+sqrt(discriminant))/(2*parametr_a)
-    if discriminant == 0:
-        return root1, None
-    else:
-        return root1, root2
-    if __name__ == '__main__':
-        solution = get_roots(parametr_a, parametr_b, parametr_c)
+        discriminant = parametr_b**2 - 4*parametr_a*parametr_c
+        if discriminant < 0:
+          root1 = None
+          root2 = None
+          return root1, root2
+        else:
+          root1 = (-parametr_b-sqrt(discriminant))/(2*parametr_a)
+          root2 = (-parametr_b+sqrt(discriminant))/(2*parametr_a)
+        if discriminant == 0:
+          return root1, None
+        else:
+          return root1, root2
+
+
+if __name__ == '__main__':
+    get_roots(a,b,c)
